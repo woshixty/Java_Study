9d893197e9de3ba1180a895bef2e89a454c7731d
woshixty <qyyzxty@icloud.com>
2020-10-07 23:28:51 +0800

    nothing

---
+++ demo/src/main/java/com/xty/demo/entity/User.java
@@ -0,0 +1,15 @@
+package com.xty.demo.entity;
+
+import lombok.Data;
+
+/**
+ * @author qyyzxty@icloud.com
+ * 2020/10/7
+ **/
+@Data
+public class User {
+    private Long id;
+    private String name;
+    private Integer age;
+    private String email;
+}
---
+++ demo/src/main/java/com/xty/demo/mybaitsPlus/test.java
@@ -0,0 +1,8 @@
+package com.xty.demo.mybaitsPlus;
+
+/**
+ * @author qyyzxty@icloud.com
+ * 2020/10/7
+ **/
+public class test {
+}
