88a484e00b069988e6b5111855a37f6716a6f62e
谢庭宇 <qyyzxty@icloud.com>
2020-10-07 23:19:10 +0800

    mybait-plus

---
+++ demo/src/main/java/com/xty/demo/mapper/UserMapper.java
@@ -0,0 +1,12 @@
+package com.xty.demo.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.xty.demo.entity.User;
+
+/**
+ * @author qyyzxty@icloud.com
+ * 2020/10/7
+ **/
+public interface UserMapper extends BaseMapper<User> {
+
+}
