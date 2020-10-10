54665f4886f91f2ee228d25694f4a0597ce7b8b1
谢庭宇 <qyyzxty@icloud.com>
2020-10-10 20:20:51 +0800

    cos_test

---
+++ demo/src/main/java/com/xty/CosCloud/filetest.java
@@ -0,0 +1,32 @@
+package com.xty.CosCloud;
+
+import org.apache.http.entity.ContentType;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.*;
+
+import org.springframework.mock.web.MockMultipartFile;
+
+/**
+ * @author qyyzxty@icloud.com
+ * 2020/10/10
+ **/
+public class filetest {
+
+    public static void main(String[] args) throws IOException {
+        String filePath = "/Users/mr.stark/Desktop/picture/picture.png";
+        File file = new File(filePath);
+        FileInputStream fileInputStream = new FileInputStream(file);
+
+        // MockMultipartFile
+        // (String name, @Nullable String originalFilename, @Nullable String contentType, InputStream contentStream)
+        // 其中originalFilename,String contentType 旧名字，类型  可为空
+        // ContentType.APPLICATION_OCTET_STREAM.toString() 需要使用HttpClient的包
+
+        MultipartFile multipartFile = new MockMultipartFile("copy"+file.getName(),
+                file.getName(),
+                ContentType.APPLICATION_OCTET_STREAM.toString(),
+                fileInputStream);
+        System.out.println(multipartFile.getName());
+    }
+}
--- demo/src/test/java/com/xty/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/xty/demo/DemoApplicationTests.java
@@ -1,13 +1,26 @@
 package com.xty.demo;
 
+import com.xty.demo.entity.User;
+import com.xty.demo.mapper.UserMapper;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.annotation.Resource;
+import java.util.List;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class DemoApplicationTests {
 
+    @Resource
+    private UserMapper userMapper;
+
     @Test
-    void contextLoads() {
+    public void select() {
+        List<User> users = userMapper.selectList(null);
+        users.forEach(System.out::println);
     }
 
 }
