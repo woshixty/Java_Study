510f52a3092424b7bf62fed9d9620615e1cb9e3a
woshixty <qyyzxty@icloud.com>
2021-07-07 00:46:28 +0800

    webflux

---
+++ SpringSecurity/securitydemo1/src/main/java/com/atguiugu/securitydemo1/config/SecurityConfigTest.java
@@ -0,0 +1,33 @@
+package com.atguiugu.securitydemo1.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+ * @author qyyzxty@icloud.com
+ * 2021/7/7
+ **/
+@Configuration
+public class SecurityConfigTest extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(password());
+    }
+
+    @Bean
+    PasswordEncoder password() {
+        return new BCryptPasswordEncoder();
+    }
+}
