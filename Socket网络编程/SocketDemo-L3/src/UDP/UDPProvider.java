b4d9358c3d0774a27bfd9b1b6d0b583cfddf48b9
woshixty <qyyzxty@icloud.com>
2021-07-20 21:49:53 +0800

    UDP

--- Socket网络编程/SocketDemo-L3/src/UDP/UDPProvider.java
+++ Socket网络编程/SocketDemo-L3/src/UDP/UDPProvider.java
@@ -1,11 +1,12 @@
 package UDP;
 
+import UDPEXPER.MessageCreator;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
-import java.net.PortUnreachableException;
 import java.net.SocketException;
 import java.nio.charset.StandardCharsets;
+import java.util.UUID;
 
 /**
  * @author qyyzxty@icloud.com
@@ -13,6 +14,7 @@ import java.nio.charset.StandardCharsets;
  * UDP提供者，用于提供服务
  **/
 public class UDPProvider {
+    /** 第一次写的时候
     public static void main(String[] args) throws IOException {
         System.out.println("UAPProvider Started");
         //作为接收者，指定一个端口用于数据接收
@@ -42,5 +44,82 @@ public class UDPProvider {
         //完成
         System.out.println("UDPProvider Finished");
         ds.close();
+    }
+    */
+    public static void main(String[] args) throws IOException {
+        //生成一份唯一标识
+        String sn = UUID.randomUUID().toString();
+        Provider provider = new Provider(sn);
+        provider.start();
+        //读取键盘任意信息后退出
+        System.in.read();
+        provider.exit();
+    }
+
+    private static class Provider extends Thread {
+        private final String sn;
+        private boolean done = false;
+        private DatagramSocket ds = null;
+
+        private Provider(String sn) {
+            this.sn = sn;
+        }
+
+        @Override
+        public void run() {
+            super.run();
+            System.out.println("UAPProvider Started");
+            try {
+                //作为接收者，指定一个端口用于数据接收
+                ds = new DatagramSocket(20000);
+                while (!done) {
+                    //构建接收实体
+                    final byte[] buf = new byte[512];
+                    DatagramPacket receivePack = new DatagramPacket(buf, buf.length);
+                    //接收
+                    ds.receive(receivePack);
+                    //打印接收到的信息与发送者的信息
+                    //发送者IP地址
+                    String ip = receivePack.getAddress().getHostAddress();
+                    int port = receivePack.getPort();
+                    int dataLen = receivePack.getLength();
+                    String data = new String(receivePack.getData(), 0, dataLen);
+                    System.out.println("UDPProvider receive from ip：" + ip
+                            + "\tport" + port + "\tdata：" + data);
+                    //解析端口号
+                    int responsePort = MessageCreator.parsePort(data);
+                    if (responsePort != -1) {
+                        //构建一份回送数据
+                        String responseData = MessageCreator.buildWithSn(sn);
+                        byte[] responseDataBytes = responseData.getBytes(StandardCharsets.UTF_8);
+                        //直接根据发送者信息创建一份回送信息
+                        DatagramPacket responsePacket = new DatagramPacket(responseDataBytes,
+                                responseDataBytes.length,
+                                receivePack.getAddress(),
+                                receivePack.getPort());
+                        ds.send(responsePacket);
+                    }
+                }
+                //完成
+                System.out.println("UDPProvider Finished");
+            } catch (SocketException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                ds.close();
+            }
+        }
+
+        private void close() {
+            if (ds != null) {
+                ds.close();
+                ds = null;
+            }
+        }
+
+        private void exit() {
+            done = true;
+        }
     }
 }
---
+++ Socket网络编程/SocketDemo-L3/src/UDPEXPER/MessageCreator.java
@@ -0,0 +1,51 @@
+package UDPEXPER;
+
+/**
+ * @author qyyzxty@icloud.com
+ * 2021/7/20
+ **/
+public class MessageCreator {
+    public static final String SN_HEADER = "收到暗号，我是（SN）：";
+    public static final String PORT_HEADER = "这是暗号，请回电端口（Port）：";
+
+    /**
+     * 基于端口号的创建
+     * @param port
+     * @return
+     */
+    public static String buildWithPort(int port) {
+        return PORT_HEADER + port;
+    }
+
+    /**
+     * 解析端口
+     * @param data
+     * @return
+     */
+    public static int parsePort(String data) {
+        if (data.startsWith(PORT_HEADER))
+            return Integer.parseInt(data.substring(PORT_HEADER.length()));
+        return -1;
+    }
+
+    /**
+     * 创建信息
+     * @param sn
+     * @return
+     */
+    public static String buildWithSn(String sn) {
+        return SN_HEADER + sn;
+    }
+
+    /**
+     * 解析信息
+     * @param data
+     * @return
+     */
+    public static String parseSn(String data) {
+        if (data.startsWith(SN_HEADER)) {
+            return data.substring(SN_HEADER.length());
+        }
+        return null;
+    }
+}
