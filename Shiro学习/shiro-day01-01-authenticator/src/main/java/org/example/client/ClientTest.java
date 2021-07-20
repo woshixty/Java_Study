9d6dce74de71474fe1ec164e8f5262fa886d84c0
woshixty <qyyzxty@icloud.com>
2021-07-20 13:44:07 +0800

    hash

--- Shiro学习/shiro-day01-01-authenticator/src/main/java/org/example/client/ClientTest.java
+++ Shiro学习/shiro-day01-01-authenticator/src/main/java/org/example/client/ClientTest.java
@@ -1,7 +1,9 @@
 package org.example.client;
 
+import org.example.tools.DigestsUtil;
 import org.example.tools.EncodesUtil;
 import org.junit.Test;
+import java.util.Map;
 
 /**
  * @author qyyzxty@icloud.com
@@ -24,5 +26,10 @@ public class ClientTest {
         String valHandler = new String(EncodesUtil.decodeBase64(flag));
         System.out.println("比较结果" + val.equals(valHandler));
         System.out.println("flag：" + flag);
+    }
+    @Test
+    public void testDigestsUtil() {
+        Map<String, String> map = DigestsUtil.entryptPassword("123");
+        System.out.println(map.toString());
     }
 }
--- Shiro学习/shiro-day01-01-authenticator/src/main/java/org/example/tools/DigestsUtil.java
+++ Shiro学习/shiro-day01-01-authenticator/src/main/java/org/example/tools/DigestsUtil.java
@@ -1,11 +1,14 @@
 package org.example.tools;
 
+import org.apache.shiro.crypto.SecureRandomNumberGenerator;
 import org.apache.shiro.crypto.hash.SimpleHash;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author qyyzxty@icloud.com
  * 2021/7/19
- * 生成摘要
+ * 生成摘要，散列算法
  **/
 public class DigestsUtil {
     //加密方式
@@ -20,5 +23,28 @@ public class DigestsUtil {
      */
     public static String shal(String input, String salt) {
         return new SimpleHash(SHA1, input, salt, ITERATION).toString();
+    }
+
+    /**
+     * 随机生成salt
+     * @return
+     */
+    public static String generateSalt() {
+        SecureRandomNumberGenerator randomNumberGenerator = new SecureRandomNumberGenerator();
+        return randomNumberGenerator.nextBytes().toHex();
+    }
+
+    /**
+     * 生成密码和salt的秘文
+     * @param passwordPlan
+     * @return
+     */
+    public static Map<String, String> entryptPassword(String passwordPlan) {
+        Map<String, String> map = new HashMap<>();
+        String salt = generateSalt();
+        String password = shal(passwordPlan, salt);
+        map.put("salt", salt);
+        map.put("password", password);
+        return map;
     }
 }
