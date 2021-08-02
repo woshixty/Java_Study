59805a6a70a042842ce2f1cfe0e8b0ea74c7b389
xty <qyyzxty@icloud.com>
2021-08-02 17:03:47 +0800

    服务端搞定

--- Socket网络编程/SocketDemo-L5-TCP-Channel/src/main/java/server/TCPServer.java
+++ Socket网络编程/SocketDemo-L5-TCP-Channel/src/main/java/server/TCPServer.java
@@ -1,17 +1,23 @@
 package server;
 
+import server.handle.ClientHandler;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TCPServer {
     //端口号
     private final int port;
     //客户端监听实体类
     private ClientListener mListener;
+    //客户端集合
+    List<ClientHandler> clientHandlerList = new ArrayList<>();
 
     public TCPServer(int port) {
         this.port = port;
@@ -35,10 +41,23 @@ public class TCPServer {
         }
     }
 
+    public void broadcast(String str) {
+        //给每个客户端发送消息
+        for (ClientHandler clientHandler : clientHandlerList) {
+            clientHandler.send(str);
+        }
+        //每个客户端退出
+        for (ClientHandler clientHandler : clientHandlerList) {
+            clientHandler.exit();
+        }
+        //清空客户端列表
+        clientHandlerList.clear();
+    }
+
     /**
      * 客户端消息监听
      */
-    private static class ClientListener extends Thread {
+    private class ClientListener extends Thread {
         private ServerSocket server;
         private boolean done = false;
 
@@ -60,10 +79,17 @@ public class TCPServer {
                 } catch (IOException e) {
                     continue;
                 }
-                // 客户端构建异步线程
-                ClientHandler clientHandler = new ClientHandler(client);
-                // 启动线程
-                clientHandler.start();
+                try {
+                    // 客户端构建异步线程
+                    ClientHandler clientHandler = new ClientHandler(client, handler -> {
+                        clientHandlerList.remove(handler);
+                    });
+                    // 读取数据并打印
+                    clientHandler.readToPrint();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    System.out.println("客户端连接异常：" + e.getMessage());
+                }
             } while (!done);
             System.out.println("服务器已关闭！");
         }
@@ -75,58 +101,6 @@ public class TCPServer {
             } catch (IOException e) {
                 e.printStackTrace();
             }
-        }
-    }
-
-    /**
-     * 客户端消息处理
-     */
-    private static class ClientHandler extends Thread {
-        private Socket socket;
-        private boolean flag = true;
-
-        ClientHandler(Socket socket) {
-            this.socket = socket;
-        }
-
-        @Override
-        public void run() {
-            super.run();
-            System.out.println("新客户端连接：" + socket.getInetAddress() +
-                    " P:" + socket.getPort());
-            try {
-                // 得到打印流，用于数据输出；服务器回送数据使用
-                PrintStream socketOutput = new PrintStream(socket.getOutputStream());
-                // 得到输入流，用于接收数据
-                BufferedReader socketInput = new BufferedReader(new InputStreamReader(
-                        socket.getInputStream()));
-                do {
-                    // 客户端拿到一条数据
-                    String str = socketInput.readLine();
-                    if ("bye".equalsIgnoreCase(str)) {
-                        flag = false;
-                        // 回送
-                        socketOutput.println("bye");
-                    } else {
-                        // 打印到屏幕。并回送数据长度
-                        System.out.println(str);
-                        socketOutput.println("回送：" + str.length());
-                    }
-                } while (flag);
-                socketInput.close();
-                socketOutput.close();
-            } catch (Exception e) {
-                System.out.println("连接异常断开");
-            } finally {
-                // 连接关闭
-                try {
-                    socket.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            System.out.println("客户端已退出：" + socket.getInetAddress() +
-                    " P:" + socket.getPort());
         }
     }
 }
