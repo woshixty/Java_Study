1e998bf05c02ac63f5168d02faeaed5c57913f07
xty <qyyzxty@icloud.com>
2021-08-02 12:53:18 +0800

    tcp udp

---
+++ Socket网络编程/SocketDemo-L5-TCP-Channel/src/main/java/client/bean/ServerInfo.java
@@ -0,0 +1,25 @@
+package client.bean;
+
+import lombok.Data;
+
+@Data
+public class ServerInfo {
+    private String sn;
+    private int port;
+    private String address;
+
+    public ServerInfo(int port, String ip, String sn) {
+        this.port = port;
+        this.address = ip;
+        this.sn = sn;
+    }
+
+    @Override
+    public String toString() {
+        return "ServerInfo{" +
+                "sn='" + sn + '\'' +
+                ", port=" + port +
+                ", address='" + address + '\'' +
+                '}';
+    }
+}
