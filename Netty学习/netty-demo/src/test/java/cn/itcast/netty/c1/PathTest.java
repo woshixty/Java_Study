0aab1b3700ff783a007c65955bf0406004953a6c
谢庭宇 <qyyzxty@icloud.com>
2022-02-10 20:22:21 +0800

    SpringCloud

---
+++ Netty学习/netty-demo/src/test/java/cn/itcast/netty/c1/PathTest.java
@@ -0,0 +1,10 @@
+package cn.itcast.netty.c1;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class PathTest {
+    public static void main(String[] args) {
+        Path source = Paths.get("Netty学习/netty-demo/data.txt");
+    }
+}
---
+++ Netty学习/netty-demo/src/test/java/cn/itcast/netty/c1/TestFilesWalkFileTree.java
@@ -0,0 +1,33 @@
+package cn.itcast.netty.c1;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class TestFilesWalkFileTree {
+    public static void main(String[] args) throws IOException {
+        m1();
+    }
+
+    private static void m1() throws IOException {
+        AtomicInteger dirCount = new AtomicInteger();
+        AtomicInteger fileCount = new AtomicInteger();
+        Files.walkFileTree(Paths.get("/Users/xietingyu"), new SimpleFileVisitor<Path>() {
+            @Override
+            public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+                System.out.println("===>" + dir);
+                dirCount.incrementAndGet();
+                return super.preVisitDirectory(dir, attrs);
+            }
+
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                fileCount.incrementAndGet();
+                return super.visitFile(file, attrs);
+            }
+        });
+        System.out.println(dirCount);
+        System.out.println(fileCount);
+    }
+}
