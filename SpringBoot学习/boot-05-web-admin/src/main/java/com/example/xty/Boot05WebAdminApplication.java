b52ffc10ed56f5340455718a5717dbd528b6bc02
谢庭宇 <qyyzxty@icloud.com>
2022-02-25 21:58:40 +0800

    nio学习-阻塞服务器

--- SpringBoot学习/boot-05-web-admin/src/main/java/com/example/xty/Boot05WebAdminApplication.java
+++ SpringBoot学习/boot-05-web-admin/src/main/java/com/example/xty/Boot05WebAdminApplication.java
@@ -2,8 +2,10 @@ package com.example.xty;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
+@ServletComponentScan(basePackages = "com.example.xty")
 public class Boot05WebAdminApplication {
 
     public static void main(String[] args) {
