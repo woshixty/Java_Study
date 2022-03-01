5c1ea54b8a6ecb292449bea66b6050ac97fd0cd0
谢庭宇 <qyyzxty@icloud.com>
2022-03-02 00:51:45 +0800

    nio学习-文件AIO

---
+++ Netty学习/netty-demo/src/test/java/cn/itcast/netty/c5/AioServer.java
@@ -0,0 +1,43 @@
+package cn.itcast.netty.c5;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.AsynchronousFileChannel;
+import java.nio.channels.CompletionHandler;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
+import static cn.itcast.netty.c1.ByteBufferUtil.debugAll;
+
+@Slf4j
+public class AioServer {
+    public static void main(String[] args) throws IOException {
+        try{
+            AsynchronousFileChannel s =
+                    AsynchronousFileChannel.open(
+                            Paths.get("word.txt"), StandardOpenOption.READ);
+            ByteBuffer buffer = ByteBuffer.allocate(2);
+            log.debug("begin...");
+            s.read(buffer, 0, null, new CompletionHandler<Integer, ByteBuffer>() {
+                @Override
+                public void completed(Integer result, ByteBuffer attachment) {
+                    log.debug("read completed...{}", result);
+                    buffer.flip();
+                    debugAll(buffer);
+                }
+
+                @Override
+                public void failed(Throwable exc, ByteBuffer attachment) {
+                    log.debug("read failed...");
+                }
+            });
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        log.debug("do other things...");
+        System.in.read();
+    }
+}
